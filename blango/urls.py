1956997f38da354625f7e5aacb5b61155b880db1
Mohamed Abdelmoneim <mohamedabdo581@gmail.com>
2024-08-26 19:48:32 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog.views import index, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path("post/<slug>/", post_detail,
-        name="blog-post-detail")
+        name="blog-post-detail"),
+    path("api/v1/", include("blog.api_urls")),
 ]
