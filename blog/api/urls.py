e34ae76ea2f1c7d2efd58a98b9e49080fa476092
Mohamed Abdelmoneim <mohamedabdo581@gmail.com>
2024-08-28 06:33:35 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
-from blog.api.views import PostList, PostDetail
+from blog.api.views import PostList, PostDetail, UserDetail
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
@@ -12,5 +12,7 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
+    path("token-auth/", views.obtain_auth_token),
+    path("users/<str:email>", UserDetail.as_view(), 
+      name="api_user_detail")
 ]
